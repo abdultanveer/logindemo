f2572c456df605e0c30219f3f15a3ae0d7c37d40
abdultanveer <abdul.tanveer@gmail.com>
2018-04-06 23:27:22 +0530

    Update LoginMVPPresenter.java

--- app/src/main/java/com/example/hefen/logindemo/ui/login/LoginMVPPresenter.java
+++ app/src/main/java/com/example/hefen/logindemo/ui/login/LoginMVPPresenter.java
@@ -1,6 +1,7 @@
 package com.example.hefen.logindemo.ui.login;
 
 public interface LoginMVPPresenter {
+    // public void onButtonClicked(View view);
     public void login_request(String mobile, String password);
     public void login_reply(String reply);
 }
